09c68742aa95458ce5f8518dc89a0028fa01d5de
Andrew <andrew123709@gmail.com>
2024-08-31 02:55:09 -0400

    styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,15 @@ export function Header() {
     return (
         <header className="w-1/2 p-4 sticky top-0 flex max-h-screen w-1/2 flex-col justify-between py-24">
             <div className="text-slate-200">
-                <p className="text-6xl font-bold">Andrew Lin</p>
-                <br/>
-                <p className="text-lg font-semibold">Computer Science Student @ University of Waterloo</p>
+                <p className="text-5xl font-bold tracking-tight">Andrew Lin</p>
+
+                <p className="mt-6 text-xl font-medium tracking-tight">Computer Science Student @ University of Waterloo</p>
             </div>
             <div className="flex">
                 <a className="text-3xl hover:text-slate-200" href={"https://github.com/andrew-33"}>
                     <FaGithub />
                 </a>
-                <div className="mx-2"></div>
+                <div className="mx-3"></div>
                 <a className="text-3xl hover:text-slate-200" href={"https://www.linkedin.com/in/andrewlin2/"}>
                     <FaLinkedin />
                 </a>
