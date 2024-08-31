48b6a4dc0f45f4617a5eebda6896fa54ebd9ee18
Andrew <andrew123709@gmail.com>
2024-08-31 01:57:38 -0400

    styling

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,6 +1,6 @@
 export function Content() {
     return (
-        <main className="w-1/2 p-4 h-full">
+        <main className="w-1/2 p-4 h-full py-24">
             <h1 className="text-xl font-bold">Scrollable Column</h1>
             <div className="space-y-4">
             {[...Array(50)].map((_, i) => (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 export function Header() {
     return (
-        <header className="w-1/2 p-4 sticky top-0 flex max-h-screen w-1/2 flex-col justify-between py-24">
-            <h1 className="text-xl font-bold">Static Column</h1>
+        <header className="w-1/2 p-4 sticky top-0 flex max-h-screen w-1/2 flex-col justify-between py-24 text-slate-200">
+            <div>
+                <p className="text-6xl font-bold">Andrew Lin</p>
+                <br/>
+                <p className="text-lg font-semibold">Computer Science Student @ University of Waterloo</p>
+            </div>
             <p>footer</p>
         </header>
     );    
