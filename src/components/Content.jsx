9c14463779390b39d6447a7b12e88a0781cff0c7
Andrew <andrew123709@gmail.com>
2024-09-03 03:41:07 -0400

    add projects

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -57,7 +57,30 @@ export function Content() {
 
             <section id="projects" className="py-16">
                 <h2 className="font-bold text-2xl uppercase tracking-wider text-slate-200">Projects</h2>
-                <p className="mt-4 text-slate-400">Your contact information here... Your contact information here... Your contact information here... Your contact information here...Your contact information here...Your contact information here...Your contact information here...Your contact information here...Your contact information here...Your contact information here... Your contact information here...</p>
+                <ProjectCard
+                    projectName="Ticket Finder"
+                    description="Live-updating dashboard to keep track of upcoming events and ticket price history"
+                    tags={['React', 'Spring Boot', 'MySQL', 'Docker']}
+                    url="https://github.com/3andrew/ticket-finder"
+                />            
+                <ProjectCard
+                    projectName="Live Transcriptions"
+                    description="Chrome extension that generates live transcripts from any web browser audio source using OpenAI's Whisper"
+                    tags={['JavaScript', 'HTML/CSS']}
+                    url="https://github.com/3andrew/live-transcriptions"
+                />  
+                <ProjectCard
+                    projectName="Portfolio Website"
+                    description="Personal website using Three JS and Tailwind CSS, deployed with Vercel"
+                    tags={['React', 'Tailwind CSS', 'Three JS']}
+                    url=""
+                />     
+                <ProjectCard
+                    projectName="External Links for Spotify"
+                    description="App that provides links to music database websites based on user's current Spotify listening activity"
+                    tags={['JavaScript', 'HTML/CSS']}
+                    url="https://github.com/3andrew/ticket-finder"
+                />       
             </section>
         </div>
     );
@@ -82,6 +105,31 @@ const JobCard = ({ imagePath, companyName, position, location, duration, tags, u
                 <div className="text-right">
                     <p className="text-lg font-semibold text-slate-200">{location}</p>
                     <p className="text-md text-slate-200">{duration}</p>
+                </div>
+            </div>
+            <div className="flex space-x-2">
+                {tags.map((tag, index) => (
+                    <span
+                        key={index}
+                        className="bg-violet-800 bg-opacity-25 text-violet-300 text-xs font-medium px-3 py-1 rounded-full"
+                    >
+                        {tag}
+                    </span>
+                ))}
+            </div>
+        </a>
+    );
+};
+
+const ProjectCard = ({ projectName, description, tags, url }) => {
+    return (
+        <a href={url} target="_blank" rel="noopener noreferrer" className="block rounded-lg p-3 my-8 hover:bg-violet-800 hover:bg-opacity-15 transition">
+            <div className={`flex justify-between items-start ${tags.length ? "mb-4" : ""}`}>
+                <div className="flex items-start">
+                    <div>
+                        <p className="text-lg font-semibold text-slate-200 mb-2">{projectName}</p>
+                        <p className="text-md text-slate-200">{description}</p>
+                    </div>
                 </div>
             </div>
             <div className="flex space-x-2">
